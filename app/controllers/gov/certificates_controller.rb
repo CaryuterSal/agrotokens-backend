c83a0c957d00f17bb49fd6243c46462ea75f226d
Caryuter <111262772+Caryuter@users.noreply.github.com.>
2025-10-30 11:02:35 -0600

    fix cert info controller

--- app/controllers/gov/certificates_controller.rb
+++ app/controllers/gov/certificates_controller.rb
@@ -3,15 +3,7 @@ class Gov::CertificatesController < ApplicationController
 
   # GET /gov/certificates/:id
   def show
-    render json: {
-      id: @certificate.id,
-      status: @certificate.status,
-      uploader_wallet: @certificate.uploader_wallet,
-      parsed_json: @certificate.parsed_json,
-      hectares: @certificate.hectares,
-      location_text: @certificate.location_text,
-      blockchain_batch_id: @certificate.blockchain_batch_id
-    }
+    render json: @certificate
   end
 
   def sign_and_submit
